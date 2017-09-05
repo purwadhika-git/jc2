53b7d88742d90d5c99f40a4704874caf29c5098b
HASBI\ACER <hasbifadillah@outlook.com>
2017-09-05 15:23:45 +0700

    Update 05-09-2017 v2

--- Front-end/todolist/src/app/app.component.ts
+++ Front-end/todolist/src/app/app.component.ts
@@ -7,8 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  ToDo : string = "";
+  ToDoList : object[] = [];
+
   AddToDo () : void {
-    alert("Halo");
+    if (this.ToDo != "") {
+      var obj = { "todo" : this.ToDo, "deleted" : false, "completed" : false };
+      this.ToDoList.push(obj);
+      this.ToDo = "";
+    }
   }
 
+  GetData(){
+    var newList : object[];
+    
+    for (var i = 0; i < this.ToDoList.length; i++) {
+      var todo = this.ToDoList[i];
+      
+      // if (todo["deleted"] == false) {
+      //   newList.push(todo);
+      // }
+      
+      if ( ! todo["deleted"]) {
+        newList.push(todo);
+      }
+
+      var angka = 10;
+      var hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+
+    }
+    
+    return newList;
+  }
+
+  DeleteToDo(index) : void {
+    this.ToDoList.splice(index, 1);
+  }
 }
