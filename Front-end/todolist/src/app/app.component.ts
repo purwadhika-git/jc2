6482521fbad115b6bf2f48f2c61616184c76e561
HASBI\ACER <hasbifadillah@outlook.com>
2017-09-08 09:14:07 +0700

    Update 08/09/2017

--- Front-end/todolist/src/app/app.component.ts
+++ Front-end/todolist/src/app/app.component.ts
@@ -9,38 +9,82 @@ export class AppComponent {
   
   ToDo : string = "";
   ToDoList : object[] = [];
+  Completed : boolean = false;
 
   AddToDo () : void {
     if (this.ToDo != "") {
-      var obj = { "todo" : this.ToDo, "deleted" : false, "completed" : false };
+
+      var obj : object;
+
+      if (this.ToDoList.length != 0) {
+        var lastId = this.ToDoList[this.ToDoList.length - 1]["id"];
+        obj = { "id" : lastId + 1,  "todo" : this.ToDo, "deleted" : false, "completed" : false };
+      }
+      else {
+        obj = { "id" : 1,  "todo" : this.ToDo, "deleted" : false, "completed" : false };
+      }
+
       this.ToDoList.push(obj);
       this.ToDo = "";
+
+      console.log(this.ToDoList);
+
     }
   }
 
-  GetData(){
-    var newList : object[];
+  GetData() : object[]{
+    var newList : object[] = [];
     
     for (var i = 0; i < this.ToDoList.length; i++) {
+      
       var todo = this.ToDoList[i];
-      
-      // if (todo["deleted"] == false) {
-      //   newList.push(todo);
-      // }
-      
       if ( ! todo["deleted"]) {
         newList.push(todo);
       }
 
-      var angka = 10;
-      var hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+      // if (todo["deleted"] == false) {
+      //   newList.push(todo);
+      // }z
 
+      // var angka = 10;
+      // var hasil = (angka % 2 == 0) ? "genap" : "ganjil";
     }
     
     return newList;
   }
 
-  DeleteToDo(index) : void {
-    this.ToDoList.splice(index, 1);
+  DeleteToDo(id) : void {
+
+    for (var i = 0; i < this.ToDoList.length; i++) {
+      if (this.ToDoList[i]["id"] == id) {
+        this.ToDoList[i]["deleted"] = true;
+        break;
+      }
+    }
+    console.log(this.ToDoList);
+    //this.ToDoList[index]["deleted"] = true;
+    //this.ToDoList.splice(index, 1);
   }
+
+  CompleteToDo(e, id){
+
+    for (var i = 0; i < this.ToDoList.length; i++) {
+      if (this.ToDoList[i]["id"] == id) {
+        
+        if (e.target.checked) {
+          this.ToDoList[i]["completed"] = true;
+          this.Completed = true;
+        }
+        else{
+          this.ToDoList[i]["completed"] = false;
+          this.Completed = false;
+        }
+        
+        break;
+      }
+    }
+
+    console.log(this.ToDoList);
+  }
+
 }
