12928f66b27c63079ef9ef93545602cac29209aa
HASBI\ACER <hasbifadillah@outlook.com>
2017-09-08 14:15:27 +0700

    Update 08/09/2017 v2

--- Front-end/calculator/src/app/app.component.ts
+++ Front-end/calculator/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  angka1 : number;
+  angka2 : number;
+  result : number;
+  history : string[] = [];
+
+  calculate(operator : string){
+    if (operator == "+") {
+      this.result = Number(this.angka1) + Number(this.angka2);
+    }
+    else if (operator == "-") {
+      this.result = Number(this.angka1) - Number(this.angka2);
+    }
+    else if (operator == "x") {
+      this.result = Number(this.angka1) * Number(this.angka2);
+    }
+    else if (operator == "/") {
+      this.result = Number(this.angka1) / Number(this.angka2);
+    }
+
+    this.history.push(this.angka1 + " " + operator + " " + this.angka2 + " = " + this.result);
+    
+  }
 }
