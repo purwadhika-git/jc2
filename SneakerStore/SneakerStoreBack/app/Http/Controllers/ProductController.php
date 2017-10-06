1ed4a55f16a6e1944b9bdfcb79dc49683227f721
HASBI\ACER <hasbifadillah@outlook.com>
2017-10-06 11:07:46 +0700

    Update 06-10-2017 v1

--- SneakerStore/SneakerStoreBack/app/Http/Controllers/ProductController.php
+++ SneakerStore/SneakerStoreBack/app/Http/Controllers/ProductController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Product;
+use App\ProductImage;
 
 class ProductController extends Controller
 {
-    function GetAllProduct()
-    {
+    function GetAllProduct(){
         $productList = DB::select(
             'select p.id, p.name, p.unit_price, pi.image
                 from products p
@@ -30,4 +30,37 @@ class ProductController extends Controller
 
         return response()->json($productList, 200);
     }
+
+    function SaveProductImage(Request $req){
+
+        DB::beginTransaction();
+        try{
+
+            $this->validate($req, [
+                'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+                'product_id' => 'required'
+            ]);
+
+            $productId = $req->input('product_id');
+            $image = $req->file('image');
+
+            $file_name = time() . '.' . $image->getClientOriginalExtension();
+            $file_path = '/img/product/' . $productId;
+            $file_destination = public_path($file_path);
+
+            $productImage = new ProductImage;
+            $productImage->product_id = $productId;
+            $productImage->image = $file_path . "/" . $file_name;
+            $productImage->default = false;
+            $productImage->save();
+            
+            DB::commit();
+
+            $image->move($file_destination, $file_name);
+        }
+        catch(\Exception $e){
+            DB::rollback();
+        }
+    }
+
 }
--- SneakerStore/SneakerStoreBack/routes/api.php
+++ SneakerStore/SneakerStoreBack/routes/api.php
@@ -27,6 +27,7 @@ Route::group(['prefix' => 'user'], function() {
 Route::group(['prefix' => 'product'], function() {
     
     Route::get('/getallproduct', 'ProductController@GetAllProduct')->middleware('jwt.auth');
+    Route::post('/saveproductimage', 'ProductController@SaveProductImage');
 
 });
 
